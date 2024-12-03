11688043356e4d65820e3de9cb69fe1eba2a4804
Math <mathis.minet@gmail.com>
2024-12-03 14:01:31 +0100

    test

--- src/Controller/PrincipalController.php
+++ src/Controller/PrincipalController.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Controller;
-
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
